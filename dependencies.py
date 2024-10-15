517ea99ec85e9062724507097689e420b3410a23
Tahir Sher <92644572+TahirSher@users.noreply.github.com>
2024-10-15 16:39:21 +0500

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -1,15 +1,1 @@
-import subprocess
-import sys
 
-def install_python_dependencies():
-    # Update pip
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
-    
-    # Install the required Python packages from requirements.txt
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-    except Exception as e:
-        print(f"Failed to install Python dependencies: {e}")
-
-if __name__ == "__main__":
-    install_python_dependencies()
