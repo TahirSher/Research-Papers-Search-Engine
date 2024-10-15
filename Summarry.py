4e979645d54eaf527aaad2b42db31220ca80e7de
Tahir Sher <92644572+TahirSher@users.noreply.github.com>
2024-10-15 19:16:23 +0500

    Update Summarry.py

--- Summarry.py
+++ Summarry.py
@@ -3,7 +3,6 @@ import requests
 import pandas as pd
 from transformers import pipeline
 import tensorflow
-import torch
 
 # Function to search CrossRef using the user's query
 def search_crossref(query, rows=10):
